3b556684c72498655e361956e5054f44d5c04942
CodingMyWolf <58061736+CodingMyWolf@users.noreply.github.com>
2019-12-10 15:44:01 -0500

    Update and rename MotorTests.py to CarCodeV4.py

--- CarCodeV4.py
+++ CarCodeV4.py
@@ -1,10 +1,10 @@
 from microbit import *
 
-display.off()
+display.off()  #Turns the microbit display off so we can use the display pins in this build. 
 
-user_input = []
+user_input = []  # This is the list of user input commands, it is empty, but it will get filled when the user uses the code
 
-gmA1 = pin0
+gmA1 = pin0  # Pin aliasing 
 bmA2 = pin1
 
 wmB1 = pin12
@@ -18,6 +18,8 @@ bLeft = pin7
 bStart = pin6
 
 
+# This turns both the motors on for a given time, while simultaiously turning the LED on - to display that it is in progress
+# It takes positive numbers for forwards, and negative numbers for Backwards
 def both_motors_on(time):
     pin13.write_digital(1)
     if time >= 0:
@@ -46,12 +48,17 @@ def both_motors_on(time):
         bmB2.write_digital(0)
     pin13.write_digital(0)
 
+    
+# This short function blinks the LED 
 def blink():
     pin13.write_digital(1)
     sleep(75)
     pin13.write_digital(0)
 
 
+# This function turns the car a given angle - if the button is pushed for 1 second, it turns at 45 degrees
+# If the button was pushed for longer than 2 seconds, it turns 90 degrees 
+# It uses positive for left turns, and negative for right turns.
 def turn(angle):
     pin13.write_digital(1)
     if angle >= 0:
@@ -69,7 +76,7 @@ def turn(angle):
         wmB1.write_digital(0)
     pin13.write_digital(0)
 
-
+# this function records the user input, and takes input of which pins to read, and what direction. - given later in the program
 def record(x, y):
 
     i = 0
@@ -79,6 +86,9 @@ def record(x, y):
         sleep(1000)
     user_input.append((y, i))
 
+    
+# This code block is the only thing running on the microbit 
+# It checks for specific pin-Highs, and reads them, and then sends them to the record function.
 
 while True:
 
@@ -91,6 +101,10 @@ while True:
     if bRight.read_digital():
         record(bRight, "R")
 
+    # This block of code is run when the start button is pushed, 
+    # It reads the list (user_input) and looks at each tuple of it.
+    # depending on what direction is assigned, it will send the time to a different function 
+    # This is the movement part.
     if bStart.read_digital():
 
         sleep(500)
